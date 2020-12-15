b347334528d6e0a21e06c62789de8cd736f6225f
Kyle Altendorf <sda@fstab.net>
2020-12-15 15:36:57 -0500

    Set 5.15.2 as default

--- src/pep517/local_backend.py
+++ src/pep517/local_backend.py
@@ -28,7 +28,7 @@ def pad_version(v, segment_count=3):
 
 
 # TODO: really doesn't seem quite proper here and probably should come
-qt_version = pad_version(os.environ.setdefault('QT_VERSION', '5.15.1'))
+qt_version = pad_version(os.environ.setdefault('QT_VERSION', '5.15.2'))
 qt_major_version = qt_version.partition('.')[0]
 
 # Inclusive of the lower bound and exclusive of the upper
