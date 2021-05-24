20a0adcac213f90a2c4bb080c8bd0451f7c0852b
Kyle Altendorf <sda@fstab.net>
2021-05-24 15:28:20 -0400

    Use published qt-applications~=2.2

--- src/pep517/local_backend.py
+++ src/pep517/local_backend.py
@@ -33,10 +33,10 @@ qt_version = pad_version(os.environ.setdefault('QT_VERSION', '6.1.0'))
 qt_major_version = qt_version.partition('.')[0]
 
 # Inclusive of the lower bound and exclusive of the upper
-qt_applications_wrapper_range = ['2', '3']
+qt_applications_wrapper_range = ['2.2', '3']
 
 # Must be False for release.  PyPI won't let you upload with a URL dependency.
-use_qt_applications_url = True
+use_qt_applications_url = False
 
 if use_qt_applications_url:
     qt_applications_url = ' @ git+https://github.com/altendky/qt-applications@main'
