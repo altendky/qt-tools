8b63c21eaa13fbcfccd8f413fd21d05f223e0d91
Kyle Altendorf <sda@fstab.net>
2023-03-26 10:00:24 -0400

    misc

--- src/pep517/local_backend.py
+++ src/pep517/local_backend.py
@@ -57,9 +57,9 @@ qt_applications_requirement = 'qt{}-applications{}{}'.format(
 )
 
 requirements = {
-    'attrs': '== 20.3',
+    'attrs': '',
     'versioneer-518': '== 0.18',
-    'wheel': '~= 0.35.1',
+    'wheel': '',
 }
 
 
